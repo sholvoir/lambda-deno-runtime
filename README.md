e067759c1a8915590763fff5c0735033af487302
sholvoir <shaw.he@hotmail.com>
2021-05-02 17:06:32 -0400

    0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # lambda-deno-runtime
+
+This is a Deno runtime for aws lambda. Deploy it as a layer for your function to run <code>./publish</code>.
+It will down Deno Linux version and build the layer .zip file and deploy it to your aws account.
+You will get the layer Arn. Add the layer Arn to your function.
+
+## requires
+* Installed and configed [aws-cli](https://aws.amazon.com/cli/).
+* Installed [deno](https://deno.land/).
+
+## how to deploy
+```bash
+./publish
+```
+
+## how code the handler
+Just like for node.js, write your ts code like this:
+```TypeScript
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "https://deno.land/x/lambda/mod.ts";
+
+export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+  return {
+    statusCode: 200,
+    headers: { "content-type": "text/html;charset=utf8" },
+    body: `Welcome to deno ${Deno.version.deno} 🦕`,
+  };
+}
+```
+use <code>deno bundle</code> to bundle your code to a sighle js file and
+tell lambda that the handler is "*yourbundlejsfilename.handler*".
+
+done. enjoy.
---
+++ runtime.ts
@@ -0,0 +1,48 @@
+async function init() {
+    const runtimeApi = `http://${Deno.env.get('AWS_LAMBDA_RUNTIME_API')}/2018-06-01/runtime`;
+    let handle;
+    try {
+        const [file, method] = Deno.env.get('_HANDLER')?.split('.') as [string, string];
+        const script = `${Deno.env.get('LAMBDA_TASK_ROOT')}/${file}.js`;
+        handle = (await import(script))[method];
+    } catch (detail) { await fetch (`${runtimeApi}/init/error`, {
+        method: 'POST',
+        headers: { 'Lambda-Runtime-Function-Error-Type': 'Unhandled' },
+        body: JSON.stringify({
+            errorMessage: 'Failed to load function.',
+            errorType: 'InvalidFunctionException',
+            detail
+        })
+    }); }
+    if (!handle) Deno.exit(-1);
+
+    const functionName = Deno.env.get('AWS_LAMBDA_FUNCTION_NAME');
+    const functionVersion = Deno.env.get('AWS_LAMBDA_FUNCTION_VERSION');
+    const memoryLimitInMB = Deno.env.get('AWS_LAMBDA_FUNCTION_MEMORY_SIZE');
+    const logGroupName = Deno.env.get('$AWS_LAMBDA_LOG_GROUP_NAME');
+    const logStreamName = Deno.env.get('$AWS_LAMBDA_LOG_STREAM_NAME');
+
+    const eventUrl = `${runtimeApi}/invocation/next`
+    while (true) {
+        const event = await fetch(eventUrl);
+        const headers = event.headers;
+        Deno.env.set('_X_AMZN_TRACE_ID', headers.get('lambda-runtime-trace-id') || '');
+        const awsRequestId = headers.get('lambda-runtime-aws-request-id');
+        const responseUrl = `${runtimeApi}/invocation/${awsRequestId}`
+        const context = {
+            functionName, functionVersion, memoryLimitInMB, logGroupName, logStreamName, awsRequestId,
+            invokedFunctionArn: headers.get('lambda-runtime-invoked-function-arn'),
+            clientContext: headers.get('lambda-runtime-client-context'),
+            cognitaIdentity: headers.get('lambda-runtime-cognito-identity'),
+            getRemainingTimeInMillis: () => (+(headers.get('lambda-runtime-deadline-ms')||0) - Date.now())
+        };
+        try {
+            const result = await handle(await event.json(), context);
+            await fetch(`${responseUrl}/response`, { method: 'POST', body: JSON.stringify(result) });
+        } catch (e) {
+            await fetch(`${responseUrl}/error`, { method: 'POST', body: JSON.stringify(e) });
+        }
+    }
+}
+
+await init();
